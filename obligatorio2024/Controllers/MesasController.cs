87af3e44dacb3d69f7dfc5c3389aed5456ce7fae
Tobías Nuñez <lpctobinz@gmail.com>
2024-06-06 15:49:58 -0300

    numero de mesa autoincrementable

--- obligatorio2024/Controllers/MesasController.cs
+++ obligatorio2024/Controllers/MesasController.cs
@@ -1,163 +1,163 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using obligatorio2024.Models;
 
-namespace obligatorio2024.Controllers
+public class MesasController : Controller
 {
-    public class MesasController : Controller
+    private readonly Obligatorio2024Context _context;
+
+    public MesasController(Obligatorio2024Context context)
     {
-        private readonly Obligatorio2024Context _context;
+        _context = context;
+    }
 
-        public MesasController(Obligatorio2024Context context)
+    // GET: Mesas
+    public async Task<IActionResult> Index()
+    {
+        var obligatorio2024Context = _context.Mesas.Include(m => m.Restaurante);
+        return View(await obligatorio2024Context.ToListAsync());
+    }
+
+    // GET: Mesas/Details/5
+    public async Task<IActionResult> Details(int? id)
+    {
+        if (id == null)
         {
-            _context = context;
+            return NotFound();
         }
 
-        // GET: Mesas
-        public async Task<IActionResult> Index()
+        var mesa = await _context.Mesas
+            .Include(m => m.Restaurante)
+            .FirstOrDefaultAsync(m => m.Id == id);
+        if (mesa == null)
         {
-            var obligatorio2024Context = _context.Mesas.Include(m => m.Restaurante);
-            return View(await obligatorio2024Context.ToListAsync());
+            return NotFound();
         }
 
-        // GET: Mesas/Details/5
-        public async Task<IActionResult> Details(int? id)
+        return View(mesa);
+    }
+
+    // GET: Mesas/Create
+    public IActionResult Create()
+    {
+        ViewData["RestauranteId"] = new SelectList(_context.Restaurantes, "Id", "Id");
+        return View();
+    }
+
+    // POST: Mesas/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("Id,NumeroMesa,Capacidad,Estado,RestauranteId")] Mesa mesa)
+    {
+        if (ModelState.IsValid)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var mesa = await _context.Mesas
-                .Include(m => m.Restaurante)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (mesa == null)
-            {
-                return NotFound();
-            }
-
-            return View(mesa);
-        }
-
-        // GET: Mesas/Create
-        public IActionResult Create()
-        {
-            ViewData["RestauranteId"] = new SelectList(_context.Restaurantes, "Id", "Id");
-            return View();
-        }
-
-        // POST: Mesas/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,NumeroMesa,Capacidad,Estado,RestauranteId")] Mesa mesa)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(mesa);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["RestauranteId"] = new SelectList(_context.Restaurantes, "Id", "Id", mesa.RestauranteId);
-            return View(mesa);
-        }
-
-        // GET: Mesas/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var mesa = await _context.Mesas.FindAsync(id);
-            if (mesa == null)
-            {
-                return NotFound();
-            }
-            ViewData["RestauranteId"] = new SelectList(_context.Restaurantes, "Id", "Id", mesa.RestauranteId);
-            return View(mesa);
-        }
-
-        // POST: Mesas/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,NumeroMesa,Capacidad,Estado,RestauranteId")] Mesa mesa)
-        {
-            if (id != mesa.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(mesa);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!MesaExists(mesa.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["RestauranteId"] = new SelectList(_context.Restaurantes, "Id", "Id", mesa.RestauranteId);
-            return View(mesa);
-        }
-
-        // GET: Mesas/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var mesa = await _context.Mesas
-                .Include(m => m.Restaurante)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (mesa == null)
-            {
-                return NotFound();
-            }
-
-            return View(mesa);
-        }
-
-        // POST: Mesas/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var mesa = await _context.Mesas.FindAsync(id);
-            if (mesa != null)
-            {
-                _context.Mesas.Remove(mesa);
-            }
-
+            mesa.NumeroMesa = _context.Mesas.Max(m => (int?)m.NumeroMesa) + 1 ?? 1;
+            _context.Add(mesa);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
+        ViewData["RestauranteId"] = new SelectList(_context.Restaurantes, "Id", "Id", mesa.RestauranteId);
+        return View(mesa);
+    }
 
-        private bool MesaExists(int id)
+    // GET: Mesas/Edit/5
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null)
         {
-            return _context.Mesas.Any(e => e.Id == id);
+            return NotFound();
         }
+
+        var mesa = await _context.Mesas.FindAsync(id);
+        if (mesa == null)
+        {
+            return NotFound();
+        }
+        ViewData["RestauranteId"] = new SelectList(_context.Restaurantes, "Id", "Id", mesa.RestauranteId);
+        return View(mesa);
+    }
+
+    // POST: Mesas/Edit/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, [Bind("Id,NumeroMesa,Capacidad,Estado,RestauranteId")] Mesa mesa)
+    {
+        if (id != mesa.Id)
+        {
+            return NotFound();
+        }
+
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                _context.Update(mesa);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MesaExists(mesa.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        ViewData["RestauranteId"] = new SelectList(_context.Restaurantes, "Id", "Id", mesa.RestauranteId);
+        return View(mesa);
+    }
+
+    // GET: Mesas/Delete/5
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var mesa = await _context.Mesas
+            .Include(m => m.Restaurante)
+            .FirstOrDefaultAsync(m => m.Id == id);
+        if (mesa == null)
+        {
+            return NotFound();
+        }
+
+        return View(mesa);
+    }
+
+    // POST: Mesas/Delete/5
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        var mesa = await _context.Mesas.FindAsync(id);
+        if (mesa != null)
+        {
+            _context.Mesas.Remove(mesa);
+            await _context.SaveChangesAsync();
+
+            // Reorganizar los números de mesa
+            var mesas = await _context.Mesas.OrderBy(m => m.NumeroMesa).ToListAsync();
+            for (int i = 0; i < mesas.Count; i++)
+            {
+                mesas[i].NumeroMesa = i + 1;
+            }
+            await _context.SaveChangesAsync();
+        }
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    private bool MesaExists(int id)
+    {
+        return _context.Mesas.Any(e => e.Id == id);
     }
 }
