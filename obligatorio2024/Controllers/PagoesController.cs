29e2831c9fa406caa8a589c2a6c6991dd93473c7
Tobías Nuñez <lpctobinz@gmail.com>
2024-06-14 06:28:45 -0300

    tipocliente automatico en pago y css de home

--- obligatorio2024/Controllers/PagoesController.cs
+++ obligatorio2024/Controllers/PagoesController.cs
@@ -9,155 +9,190 @@ using obligatorio2024.Models;
 
 namespace obligatorio2024.Controllers
 {
-    public class PagoesController : Controller
+    namespace obligatorio2024.Controllers
     {
-        private readonly Obligatorio2024Context _context;
-
-        public PagoesController(Obligatorio2024Context context)
+        public class PagoesController : Controller
         {
-            _context = context;
-        }
+            private readonly Obligatorio2024Context _context;
 
-        // GET: Pagoes
-        public async Task<IActionResult> Index()
-        {
-            var obligatorio2024Context = _context.Pagos.Include(p => p.Reserva);
-            return View(await obligatorio2024Context.ToListAsync());
-        }
-
-        // GET: Pagoes/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
+            public PagoesController(Obligatorio2024Context context)
             {
-                return NotFound();
+                _context = context;
             }
 
-            var pago = await _context.Pagos
-                .Include(p => p.Reserva)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (pago == null)
+            // GET: Pagoes
+            public async Task<IActionResult> Index()
             {
-                return NotFound();
+                var obligatorio2024Context = _context.Pagos.Include(p => p.Reserva).ThenInclude(r => r.Cliente);
+                return View(await obligatorio2024Context.ToListAsync());
             }
 
-            return View(pago);
-        }
-
-        // GET: Pagoes/Create
-        public IActionResult Create()
-        {
-            ViewData["ReservaId"] = new SelectList(_context.Reservas, "Id", "Id");
-            return View();
-        }
-
-        // POST: Pagoes/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,ReservaId,Monto,FechaPago,MetodoPago,Moneda,TipoCambio")] Pago pago)
-        {
-            if (ModelState.IsValid)
+            // GET: Pagoes/Details/5
+            public async Task<IActionResult> Details(int? id)
             {
-                _context.Add(pago);
+                if (id == null)
+                {
+                    return NotFound();
+                }
+
+                var pago = await _context.Pagos
+                    .Include(p => p.Reserva)
+                    .ThenInclude(r => r.Cliente)
+                    .FirstOrDefaultAsync(m => m.Id == id);
+                if (pago == null)
+                {
+                    return NotFound();
+                }
+
+                return View(pago);
+            }
+
+            // GET: Pagoes/Create
+            public IActionResult Create()
+            {
+                ViewData["ReservaId"] = new SelectList(_context.Reservas, "Id", "Id");
+                return View();
+            }
+
+            // POST: Pagoes/Create
+            [HttpPost]
+            [ValidateAntiForgeryToken]
+            public async Task<IActionResult> Create([Bind("Id,ReservaId,Monto,FechaPago,MetodoPago,Moneda,TipoCambio")] Pago pago)
+            {
+                if (ModelState.IsValid)
+                {
+                    // Obtener la reserva y el cliente asociado para aplicar el descuento
+                    var reserva = await _context.Reservas
+                        .Include(r => r.Cliente)
+                        .FirstOrDefaultAsync(r => r.Id == pago.ReservaId);
+
+                    if (reserva != null && reserva.Cliente != null)
+                    {
+                        pago.Monto = AplicarDescuento(pago.Monto, reserva.Cliente.TipoCliente);
+                    }
+
+                    _context.Add(pago);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                ViewData["ReservaId"] = new SelectList(_context.Reservas, "Id", "Id", pago.ReservaId);
+                return View(pago);
+            }
+
+            // GET: Pagoes/Edit/5
+            public async Task<IActionResult> Edit(int? id)
+            {
+                if (id == null)
+                {
+                    return NotFound();
+                }
+
+                var pago = await _context.Pagos.FindAsync(id);
+                if (pago == null)
+                {
+                    return NotFound();
+                }
+                ViewData["ReservaId"] = new SelectList(_context.Reservas, "Id", "Id", pago.ReservaId);
+                return View(pago);
+            }
+
+            // POST: Pagoes/Edit/5
+            [HttpPost]
+            [ValidateAntiForgeryToken]
+            public async Task<IActionResult> Edit(int id, [Bind("Id,ReservaId,Monto,FechaPago,MetodoPago,Moneda,TipoCambio")] Pago pago)
+            {
+                if (id != pago.Id)
+                {
+                    return NotFound();
+                }
+
+                if (ModelState.IsValid)
+                {
+                    try
+                    {
+                        // Obtener la reserva y el cliente asociado para aplicar el descuento
+                        var reserva = await _context.Reservas
+                            .Include(r => r.Cliente)
+                            .FirstOrDefaultAsync(r => r.Id == pago.ReservaId);
+
+                        if (reserva != null && reserva.Cliente != null)
+                        {
+                            pago.Monto = AplicarDescuento(pago.Monto, reserva.Cliente.TipoCliente);
+                        }
+
+                        _context.Update(pago);
+                        await _context.SaveChangesAsync();
+                    }
+                    catch (DbUpdateConcurrencyException)
+                    {
+                        if (!PagoExists(pago.Id))
+                        {
+                            return NotFound();
+                        }
+                        else
+                        {
+                            throw;
+                        }
+                    }
+                    return RedirectToAction(nameof(Index));
+                }
+                ViewData["ReservaId"] = new SelectList(_context.Reservas, "Id", "Id", pago.ReservaId);
+                return View(pago);
+            }
+
+            // GET: Pagoes/Delete/5
+            public async Task<IActionResult> Delete(int? id)
+            {
+                if (id == null)
+                {
+                    return NotFound();
+                }
+
+                var pago = await _context.Pagos
+                    .Include(p => p.Reserva)
+                    .ThenInclude(r => r.Cliente)
+                    .FirstOrDefaultAsync(m => m.Id == id);
+                if (pago == null)
+                {
+                    return NotFound();
+                }
+
+                return View(pago);
+            }
+
+            // POST: Pagoes/Delete/5
+            [HttpPost, ActionName("Delete")]
+            [ValidateAntiForgeryToken]
+            public async Task<IActionResult> DeleteConfirmed(int id)
+            {
+                var pago = await _context.Pagos.FindAsync(id);
+                if (pago != null)
+                {
+                    _context.Pagos.Remove(pago);
+                }
+
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["ReservaId"] = new SelectList(_context.Reservas, "Id", "Id", pago.ReservaId);
-            return View(pago);
-        }
 
-        // GET: Pagoes/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
+            private bool PagoExists(int id)
             {
-                return NotFound();
+                return _context.Pagos.Any(e => e.Id == id);
             }
 
-            var pago = await _context.Pagos.FindAsync(id);
-            if (pago == null)
+            private decimal AplicarDescuento(decimal monto, string tipoCliente)
             {
-                return NotFound();
-            }
-            ViewData["ReservaId"] = new SelectList(_context.Reservas, "Id", "Id", pago.ReservaId);
-            return View(pago);
-        }
-
-        // POST: Pagoes/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,ReservaId,Monto,FechaPago,MetodoPago,Moneda,TipoCambio")] Pago pago)
-        {
-            if (id != pago.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
+                switch (tipoCliente)
                 {
-                    _context.Update(pago);
-                    await _context.SaveChangesAsync();
+                    case "Frecuente":
+                        return monto * 0.90m; // 10% de descuento
+                    case "VIP":
+                        return monto * 0.80m; // 20% de descuento
+                    default:
+                        return monto; // Sin descuento para nuevos clientes
                 }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!PagoExists(pago.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
             }
-            ViewData["ReservaId"] = new SelectList(_context.Reservas, "Id", "Id", pago.ReservaId);
-            return View(pago);
-        }
-
-        // GET: Pagoes/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var pago = await _context.Pagos
-                .Include(p => p.Reserva)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (pago == null)
-            {
-                return NotFound();
-            }
-
-            return View(pago);
-        }
-
-        // POST: Pagoes/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var pago = await _context.Pagos.FindAsync(id);
-            if (pago != null)
-            {
-                _context.Pagos.Remove(pago);
-            }
-
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool PagoExists(int id)
-        {
-            return _context.Pagos.Any(e => e.Id == id);
         }
     }
+
 }
--- obligatorio2024/Models/Obligatorio2024Context.cs
+++ obligatorio2024/Models/Obligatorio2024Context.cs
@@ -41,9 +41,9 @@ public partial class Obligatorio2024Context : DbContext
 
     public virtual DbSet<Usuario> Usuarios { get; set; }
 
-//    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-//        => optionsBuilder.UseSqlServer("Data Source=GOHANSSJ2; Initial Catalog=obligatorio2024; Integrated Security=true; TrustServerCertificate=True");
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=GOHANSSJ2; Initial Catalog=obligatorio2024; Integrated Security=true; TrustServerCertificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -167,7 +167,7 @@ public partial class Obligatorio2024Context : DbContext
 
         modelBuilder.Entity<Pago>(entity =>
         {
-            entity.HasKey(e => e.Id).HasName("PK__Pagos__3214EC27FEB9698B");
+            entity.HasKey(e => e.Id).HasName("PK__Pagos__3214EC27B323BD92");
 
             entity.Property(e => e.Id).HasColumnName("ID");
             entity.Property(e => e.FechaPago).HasColumnType("datetime");
@@ -183,7 +183,6 @@ public partial class Obligatorio2024Context : DbContext
 
             entity.HasOne(d => d.Reserva).WithMany(p => p.Pagos)
                 .HasForeignKey(d => d.ReservaId)
-                .OnDelete(DeleteBehavior.Cascade)
                 .HasConstraintName("FK_Pagos_Reservas");
         });
 
